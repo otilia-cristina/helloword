6cb4260752b69a8ddd8cd1a0a6177a7451e5d962
otilia-cristina <101247225+otilia-cristina@users.noreply.github.com>
2022-03-09 10:44:22 +0200

    schimbare

--- src/laborator2/clasa1.java
+++ src/laborator2/clasa1.java
@@ -5,6 +5,8 @@ public class clasa1 {
 	public static void main(String[] args) {
 		System.out.println("hello");
         System.out.println("portocale");
+		 System.out.println("atat se poate");
+		// hiiis
 	}
 
 }
